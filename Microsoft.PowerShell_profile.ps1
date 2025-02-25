f837c066799c2501123763ffa1b612e12d8bed70
Hibrahim G. Corona <hibrahim.corona@outlook.com>
2025-02-25 13:47:26 -0800

    Added funtions: - Added new funtion: Pull-Branch.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,7 @@ function gss { git status -s }
 function ga { git add . }
 function gcom { git commit -m $args }
 function glog { git log --oneline }
+function nano ($File){ bash -c "nano $File" }
 
 ###     PSReadLineOptions
 $PSReadLineOptions = @{
@@ -23,3 +24,20 @@ Set-PSReadLineOption @PSReadLineOptions
 
 ###     Pimp my terminal
 oh-my-posh init pwsh --config $env:USERPROFILE'\AppData\Local\Programs\oh-my-posh\themes\gruvbox.omp.json' | Invoke-Expression
+
+###    Functions
+function Pull-Branch {
+    param (
+        [string]$rootPath,
+        [string]$branch = 'main'
+    )
+
+    Get-ChildItem -Path $rootPath -Directory -Recurse | ForEach-Object {
+        if (Test-Path "$($_.FullName)\.git") {
+            Write-Host "Pulling $branch branch in $($_.FullName)"
+            Push-Location $_.FullName
+            git pull origin $branch
+            Pop-Location
+        }
+    }
+}
