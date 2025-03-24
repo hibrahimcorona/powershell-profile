c3d562254282ecac5a9ab6b2bdb4a701e2f8930d
Hibrahim G. Corona <hibrahim.corona@outlook.com>
2025-03-24 10:41:33 -0700

    Modified the Clear-NonExistentBranches funtion: - Added more logging. - Changed the way to get and delete the branches.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,15 +9,15 @@ function gss { git status -s }
 function ga { git add . }
 function gcom { git commit -m $args }
 function glog { git log --oneline }
-function nano ($File){ bash -c "nano $File" }
+function nano ($File) { bash -c "nano $File" }
 
 ###     PSReadLineOptions
 $PSReadLineOptions = @{
-    EditMode = 'Windows'
-    HistoryNoDuplicates = $true
+    EditMode                      = 'Windows'
+    HistoryNoDuplicates           = $true
     HistorySearchCursorMovesToEnd = $true
-    PredictionSource = 'History'
-    PredictionViewStyle = 'ListView'
+    PredictionSource              = 'History'
+    PredictionViewStyle           = 'ListView'
 }
 
 Set-PSReadLineOption @PSReadLineOptions
@@ -47,13 +47,41 @@ function Pull-Branch {
 
 # Crate a new function that will clear the branches that are non-existent in the remote repository.
 function Clear-NonExistentBranches {
-    Write-Host "Clearing non-existent branches." -ForegroundColor Yellow
+    # Write-Host "Clearing non-existent branches." -ForegroundColor Yellow
     
-    git fetch --prune
-    git branch -vv | Where-Object { $_ -match ": gone]" } | ForEach-Object {
-        $branch = $_ -replace '\s+.*$'
-        git branch -D $branch
+    # git fetch --prune
+    # git branch -vv | Where-Object { $_ -match ": gone]" } | ForEach-Object {
+    #     $branch = $_ -replace '\s+.*$'
+    #     git branch -D $branch
 
-        Write-Host "Branch $branch has been deleted." -ForegroundColor Yellow
+    #     Write-Host "Branch $branch has been deleted." -ForegroundColor Yellow
+    # }
+
+    # Ensure the script stops on errors
+    $ErrorActionPreference = "Stop"
+
+    # Fetch the latest remote branches
+    Write-Output "Fetching latest branches from remote..."
+    git fetch --prune
+
+    # Get all local branches
+    $localBranches = git branch | ForEach-Object { $_.Trim() }
+
+    # Get all remote branches
+    $remoteBranches = git branch -r | ForEach-Object { $_.Trim() -replace '^origin/', '' }
+
+    # Identify local branches that do not exist on remote
+    $branchesToDelete = $localBranches | Where-Object { $_ -ne "main" -and $_ -ne "master" -and $_ -notin $remoteBranches }
+
+    if ($branchesToDelete) {
+        Write-Output "Deleting the following branches:"
+        $branchesToDelete | ForEach-Object { Write-Output $_ }
+
+        # Delete each stale branch
+        $branchesToDelete | ForEach-Object { git branch -D $_ }
     }
+    else {
+        Write-Output "No local branches need to be deleted."
+    }
+
 }
